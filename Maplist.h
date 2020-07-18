b0cbaca6dba658c487d050302335937166fcb5cf
a-new-b <a1483423207@163.com>
2020-07-18 14:02:26 +0800

    frist commit 2020-7-18

---
+++ Maplist.h
@@ -0,0 +1,219 @@
+﻿#pragma once
+#include <stdexcept>
+#include <iostream>
+#include <vector>
+
+
+using std::vector;
+using std::cout;
+
+template <class T>
+struct mapNode
+{
+	T element;                      //节点内储存的数据
+	mapNode<T>* next;				//下一个节点
+	//构造函数
+	mapNode<T>() {};
+	mapNode<T>(const T& eelement, mapNode<T>* nnext = nullptr) {
+		this->element = eelement; this->next = nnext;
+	}
+};
+
+
+template <class T>
+class mapList
+{
+private:
+	int listSize;          //链长
+	mapNode<T>* firstNode;		//头结点。注意！此链表结构头结点即存放第一个数据！
+public:
+	mapList();						//构造函数
+	~mapList() {};					//析构函数
+	void createMapList(vector<T> a);        //用vector数组创建链表
+	inline mapNode<T>* getFirstNode() const { return firstNode; }	//返回头结点
+	inline int getSize() const { return listSize; }		//返回长度
+	void changeFistNode(mapNode<T>* newFirstNode);
+	void insertMaplist(int theIndex, const T& theElement);  //以0为起始下标插入数据
+	void deleteMapListByIndex(int theIndex);		//删除下标为 theInex的节点
+	void deleteMapListByElement(const T& theElement);	//删除数据等于theElement的节点
+	void pushBack(const T& theElement);  //在最后方增加新的节点
+	void pop();  //删除最后一个节点
+	mapList(const mapList<T>& theList);  //复制链表
+	void destoryMaplist();    //销毁链表
+	inline bool empty() const { return listSize == 0; } //返回bool值表达该链表是否为空
+	vector<int> indexOf(const T& theElement) const; //返回数据为theElement的节点的下标。
+};
+
+template<class T>
+mapList<T>::mapList()
+{
+	this->firstNode = nullptr;
+	this->listSize = 0;
+}
+
+template<class T>
+void mapList<T>::createMapList(vector<T> a)
+{
+	int N = a.size();
+	this->listSize = N;
+	if (!N) {
+		this->firstNode = nullptr;
+		this->listSize = 0;
+		return;
+	}
+	this->firstNode = new mapNode<T>(a[0]);
+	mapNode<T>* p = this->firstNode;
+	for (size_t i = 1; i < N; i++) {
+		p->next = new mapNode<T>(a[i]);
+		p = p->next;
+	}
+}
+
+
+template<class T>
+mapList<T>::mapList(const mapList<T>& theList) //复制函数
+{
+	this->listSize = theList.listSize;
+	if (!this->listSize) {
+		this->firstNode = nullptr;
+		return;
+	}
+	mapNode<T>* sourceNode = theList.firstNode;
+	mapNode<T>* p = new mapNode<T>(theList.firstNode->element);
+	sourceNode = sourceNode->next;
+	this->firstNode = p;
+	mapNode<T>* q = theList.firstNode;
+	while (q->next)
+	{
+		p->next = q->next;
+		p = p->next;
+		q = q->next;
+	}
+	p->next = nullptr;
+}
+
+template<class T>
+void mapList<T>::destoryMaplist() {
+	while (this->firstNode)
+	{
+		mapNode<T>* nextNode = firstNode->next;
+		free(this->firstNode);
+		this->firstNode = nextNode;
+	}
+	this->~mapList();
+}
+
+template<class T>
+vector<int> mapList<T>::indexOf(const T& theElement) const
+{
+	vector<int> ans = {};
+	int ct = 0;
+	auto i = this->firstNode;
+	while (i)
+	{
+		if (i->element == theElement)
+			ans.push_back(ct);
+		i = i->next;
+		ct++;
+	}
+	return ans;
+}
+
+
+template<class T>
+inline void mapList<T>::pop()
+{
+	if (!this->listSize)
+		throw  "the index out of range";
+	else if (this->listSize-- == 1)
+		this->firstNode = nullptr;
+	else {
+		mapNode<int>* p = this->firstNode;
+		while (p->next->next)
+			p = p->next;
+		free(p->next);
+		p->next = nullptr;
+	}
+}
+
+template<class T>
+void mapList<T>::pushBack(const T& theElement)
+{
+	if (!this->listSize++) {
+		this->firstNode = new mapNode<T>(theElement);
+		return;
+	}
+	mapNode<T>* newone = new mapNode<T>(theElement);
+	mapNode<T>* p = this->firstNode;
+	while (p->next)
+		p = p->next;
+	p->next = newone;
+}
+
+
+template<class T>
+inline void mapList<T>::changeFistNode(mapNode<T>* newFirstNode)
+{
+	newFirstNode->next = this->firstNode;
+	this->firstNode = newFirstNode;
+}
+
+template<class T>
+inline void mapList<T>::insertMaplist(int theIndex, const T& theElement)
+{
+	if (theIndex < 0 || theIndex > this->listSize) {
+		throw  "the index out of range";
+	}
+	this->listSize++;
+	int i = 0;
+	if (!theIndex) {
+		mapNode<T>* temp = new mapNode<int>(theElement);
+		temp->next = this->firstNode;
+		this->firstNode = temp;
+		return;
+	}
+	mapNode<T>* temp = this->firstNode;
+	for (int i = 0; i < theIndex - 1; i++)
+		temp = temp->next;
+	mapNode<T>* newone = new mapNode<int>(theElement);
+	newone->next = temp->next;
+	temp->next = newone;
+}
+
+template<class T>
+void mapList<T>::deleteMapListByIndex(int theIndex)
+{
+	if (theIndex < 0 || theIndex >= this->listSize)
+		throw "theIndex erro";
+	if (!theIndex) {
+		auto p = this->firstNode;
+		this->firstNode = this->firstNode->next;
+		free(p);
+		return;
+	}
+	auto p = this->firstNode;
+	for (int i = 0; i < theIndex; i++)
+		p = p->next;
+	p->next = p->next->next;
+}
+
+template<class T>
+void mapList<T>::deleteMapListByElement(const T& theElement)
+{
+	while (this->firstNode->element == theElement)
+	{
+		auto temp = this->firstNode;
+		this->firstNode = temp->next;
+		free(temp);
+	}
+	auto p = this->firstNode;
+	while (p->next)
+	{
+		if (p->next->element == theElement) {
+			auto temp = p->next;
+			p->next = temp->next;
+			free(temp);
+		}
+		p = p->next;
+	}
+}
---
+++ Sort.h
@@ -0,0 +1,343 @@
+﻿#pragma once
+#include "Maplist.h"
+
+using std::vector;
+
+
+template<class T>
+class mmergeSort
+{
+public:
+	mmergeSort(vector<mapNode<T>*>& a) {
+		this->temp.resize(a.size());
+		mergeSortSubInterval(a, 0, a.size() - 1);
+	}
+	static void mergeSortSubInterval(vector<mapNode<T>*>& a, int low, int high) {
+		if (high <= low)
+			return;
+		int mid = (high + low) / 2;
+		mergeSortSubInterval(a, low, mid);
+		mergeSortSubInterval(a, mid + 1, high);
+		Merge(a, low, mid, high);
+	}
+	static void Merge(vector<mapNode<T>*>& a, int low, int mid, int high) {
+		int i = low, j = mid + 1;
+		for (int k = low; k <= high; k++)
+			temp[k] = a[k];
+		if (low == 0 && high == 8)
+			int kkki = 1;
+		for (int k = low; k <= high; k++) {
+			if (i > mid) a[k] = temp[j++];
+			else if (j > high) a[k] = temp[i++];
+			else if ((temp[i]->element) < (temp[j]->element)) a[k] = temp[i++];
+			else   a[k] = temp[j++];
+		}
+	}
+private:
+	static vector<mapNode<T>*> temp;
+};
+template<class T>
+vector<mapNode<T>*> mmergeSort<T>::temp = {};
+
+
+template<class T>
+class qquickSort {
+public:
+	qquickSort(vector<mapNode<T>*>& a) {
+		sort(a, 0, a.size() - 1);
+	}
+
+	static	void sort(vector<mapNode<T>*>& a, int low, int high) {
+		if (high <= low)
+			return;
+		int j = partition(a, low, high);
+		sort(a, low, j - 1);
+		sort(a, j + 1, high);
+	}
+
+	static int partition(vector<mapNode<T>*>& a, int low, int high) {
+		int i = low, j = high + 1;
+		mapNode<T>*  v = a[low];
+		while (1)
+		{
+			while ((a[++i]->element) < (v->element))
+				if (i == high)
+					break;
+			while ((v->element) < (a[--j]->element))
+				if (j == low)
+					break;
+			if (i >= j)
+				break;
+			mapNode<T>* temp = a[i];
+			a[i] = a[j];
+			a[j] = temp;
+		}
+		mapNode<T>* temp = a[low];
+		a[low] = a[j];
+		a[j] = temp;
+		return j;
+	}
+};
+
+template<class T>
+class qquickSort1 {    //dijkstra提出的快排方案，效率比快排低不少
+public:
+	qquickSort1(vector<mapNode<T>*>& a) {
+		sort(a, 0, a.size() - 1);
+	}
+
+	static void sort(vector<mapNode<T>*>& a, int low, int high) {
+		if (high <= low)
+			return;
+		int lt = low, i = low + 1, gt = high;
+		mapNode<T>* v = a[low];
+		while (i <= gt)
+		{
+			if ((a[i]->element) < (v->element))
+			{
+				mapNode<T>* temp = a[lt];
+				a[lt] = a[i];
+				a[i] = temp;
+				lt++, i++;
+			}
+			else if ((a[i]->element) > (v->element))
+			{
+				mapNode<T>* temp = a[i];
+				a[i] = a[gt];
+				a[gt] = temp;
+				gt--;
+			}
+			else
+				i++;
+		}
+		sort(a, low, lt - 1);
+		sort(a, gt + 1, high);
+	}
+};
+
+
+template<class T>
+class hheapSort {    //堆排序
+public:
+	hheapSort(vector<mapNode<T>*>& a) {
+		int N = a.size();
+		N--;
+		for (int k = N / 2 + 2; k >= 0; k--)
+			sink(a, k, N);
+		while (N > 0) {
+			mapNode<T>* temp = a[N];
+			a[N] = a[0];
+			a[0] = temp;
+			N--;
+			sink(a, 0, N);
+		}
+	}
+
+	static void sink(vector<mapNode<T>*>& a, int k, int N) {
+		while (2 * k + 1 < N)
+		{
+			int j = 2 * k + 1;
+			if (j + 1 < N && ((a[j]->element) < (a[j + 1]->element)))
+				j++;
+			if ((a[k]->element) >= (a[j]->element))
+				break;
+			mapNode<T>* temp = a[k];
+			a[k] = a[j];
+			a[j] = temp;
+			k = j;
+		}
+	}
+};
+
+template<class T>
+class Sort
+{
+public:
+	static void  changeChain(mapList<T>& object, vector<mapNode<T>*> a, int N);  //用vector记录的指针重新编排链表
+	void bubbleSort(mapList<T>& object);		//冒泡排序
+	void bubbleSort1(mapList<T>& object);     //之前写的冒泡排序版本，因为进行的交换次数过多效率不如bubbleSort
+	void insertSort(mapList<T>& object);		//插入排序		
+	void shellSort(mapList<T>& object);			//希尔排序
+	void mergeSort(mapList<T>& object);			//归并排序
+	void quickSort(mapList<T>& object);			//快速排序
+	void quickSort1(mapList<T>& object);		//据说为Dijkstra改进的快速排序，但实测效率不增反降
+	void heapSort(mapList<T>& object);			//堆排序
+};
+
+template<class T>
+inline void Sort<T>::changeChain(mapList<T>& object, vector<mapNode<T>*> a, int N)
+{
+	object.changeFistNode(a[0]);
+	mapNode<T>* temp = object.getFirstNode();
+	for (size_t i = 1; i < N; i++)
+	{
+		temp->next = a[i];
+		temp = temp->next;
+	}
+	temp->next = nullptr;
+}
+
+template<class T>
+void Sort<T>::bubbleSort(mapList<T>& object)
+{
+	vector<mapNode<T>*> a = {};
+	auto p = object.getFirstNode();
+	while (p)
+	{
+		a.push_back(p);
+		p = p->next;
+	}
+	int N = object.getSize();    //  a的长度 
+	for (size_t i = 0; i < N; i++) {
+		int index = i;
+		for (size_t j = i + 1; j < N; j++) {
+			if (a[j]->element < a[index]->element)      //记录下每个i后方最小的a[j]的坐标
+				index = j;
+		}
+		mapNode<T>* temp = a[index];
+		a[index] = a[i];
+		a[i] = temp;			//然后进行交换
+	}
+	changeChain(object, a, N);
+}
+
+template<class T>
+void Sort<T>::bubbleSort1(mapList<T>& object)
+{
+	vector<mapNode<T>*> a = {};
+	auto p = object.getFirstNode();
+	while (p)
+	{
+		a.push_back(p);
+		p = p->next;
+	}
+	int N = a.size();    //  a的长度 
+	for (size_t i = 0; i < N; i++) {
+		for (size_t j = i + 1; j < N; j++) {
+			if ((a[j]->element) < (a[i]->element)) {
+				mapNode<T>* temp = a[j];
+				a[j] = a[i];
+				a[i] = temp;
+			}
+		}
+	}
+	changeChain(object, a, N);
+}
+
+template<class T>
+void Sort<T>::bubbleSort2(mapList<T>& object)
+{
+}
+
+template<class T>
+void Sort<T>::insertSort(mapList<T>& object)
+{
+	vector<mapNode<T>*> a;
+	auto p = object.getFirstNode();
+	while (p)
+	{
+		a.push_back(p);
+		p = p->next;
+	}
+	int N = object.getSize();    //  a的长度 ;
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = i; (j > 0) && ((a[j]->element) < (a[j - 1]->element)); j--){//将每个a[j]往后插到合适的位置
+			mapNode<T>* temp = a[j];
+				a[j] = a[j - 1];
+				a[j - 1] = temp;
+		}
+	}
+	changeChain(object, a, N);
+}
+
+template<class T>
+void Sort<T>::shellSort(mapList<T>& object)
+{
+	vector<mapNode<T>*> a;
+	auto p = object.getFirstNode();
+	while (p)
+	{
+		a.push_back(p);
+		p = p->next;
+	}
+	int N = object.getSize();    //  a的长度 ;
+	int h = 1;
+	while (h < N / 3)
+		h = 3 * h + 1;           //找到合适的初始值h
+	while (h >= 1) {			//不断缩小h，对每个间隔h的小区间进行排序	
+		for (int i = h; i < N; i++)
+		{
+			for (int j = i; j >= h && ((a[j]->element) < (a[j - h]->element)); j -= h)
+			{
+				mapNode<T>* temp = a[j];
+				a[j] = a[j - h];
+				a[j - h] = temp;
+			}
+		}
+		h = h / 3;
+	}
+	changeChain(object, a, N);
+}
+
+template<class T>
+void Sort<T>::mergeSort(mapList<T>& object)
+{
+	vector<mapNode<T>*> a;
+	int N = object.getSize();
+	auto p = object.getFirstNode();
+	while (p)
+	{
+		a.push_back(p);
+		p = p->next;
+	}
+	mmergeSort<T> yy(a);
+	changeChain(object, a, N);
+}
+
+template<class T>
+void Sort<T>::quickSort(mapList<T>& object)
+{
+	vector<mapNode<T>*> a;
+	int N = object.getSize();
+	auto p = object.getFirstNode();
+	while (p)
+	{
+		a.push_back(p);
+		p = p->next;
+	}
+	qquickSort<T>  yy(a);
+	changeChain(object, a, N);
+
+}
+
+template<class T>
+void Sort<T>::quickSort1(mapList<T>& object)
+{
+	vector<mapNode<T>*> a;
+	int N = object.getSize();
+	auto p = object.getFirstNode();
+	while (p)
+	{
+		a.push_back(p);
+		p = p->next;
+	}
+	qquickSort1<T>  yy(a);
+	changeChain(object, a, N);
+}
+
+template<class T>
+void Sort<T>::heapSort(mapList<T>& object)
+{
+	vector<mapNode<T>*> a;
+	int N = object.getSize();
+	auto p = object.getFirstNode();
+	while (p)
+	{
+		a.push_back(p);
+		p = p->next;
+	}
+	hheapSort<T>  yy(a);
+	changeChain(object, a, N);
+}
+
