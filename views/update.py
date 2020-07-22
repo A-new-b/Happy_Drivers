2ddddfaad8a706eb578784ea9c3d843e0a7f7e75
a-new-b <a1483423207@163.com>
2020-07-22 14:35:08 +0800

    fix

--- views/update.py
+++ views/update.py
@@ -10,15 +10,14 @@ Update = flask.Blueprint("Update", __name__)
 def upload():
     try:
         f = flask.request.files['file']
-        g = f
         basePath = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))  # 当前文件上级目录
         upload_path = os.path.join(basePath, 'models', 'Test.dat')
         f.save(upload_path)
         result = interface.Test()
         print(result)
         if result == "suc":
-            upload_path = os.path.join(basePath, 'models', 'GTBL.dat')
-            g.save(upload_path)
+            path = os.path.join(basePath, 'models', 'GTBL.dat')
+            os.rename(upload_path,path)
             return {"message": "success"}
         else:
             return flask.make_response(json.dumps({"message": "文件错误"}), 403)
