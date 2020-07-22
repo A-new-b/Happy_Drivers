88d6359ceaed130eb47a59b9ebd0dc91aa576518
a-new-b <a1483423207@163.com>
2020-07-22 14:16:48 +0800

    fix

--- models/test.py
+++ models/test.py
@@ -2,5 +2,6 @@ from models import interface
 # print(interface.SortMethodGet())
 # print(interface.ExecSort(7))
 # print(interface.init_r())
-print(interface.DataSearch(62333))
+# print(interface.DataSearch(62333))
 # print(interface.GetRecordCount())
+print(interface.DataGetEx(1, 20))
