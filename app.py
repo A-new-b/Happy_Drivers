b3b23bb2408fc648b05c0dd9fccf63a69f9bfc6c
a-new-b <a1483423207@163.com>
2020-07-23 03:34:04 +0800

    fixed

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from views.data import Data
 from views.sort import Sort
 from views.update import Update
 from views.search import Search
-
+from views.map import Map
 app = Flask(__name__)
 
 
@@ -11,6 +11,7 @@ app.register_blueprint(Data)
 app.register_blueprint(Sort)
 app.register_blueprint(Update)
 app.register_blueprint(Search)
+app.register_blueprint(Map)
 
 if __name__ == '__main__':
     app.run()
--- views/test.py
+++ views/test.py
@@ -1,7 +1,6 @@
 # list_s = ["金门大桥", "帝国大厦", "水晶宫", "洛克菲勒中心", "白宫", "总统山", "自由女神像", "圣路易斯拱门", "布鲁克林桥"]
 from views import map
 from models import wolframLink
-import json
 
 list_data = [
     {"linkId": 1234},
